ffc19ebd296c53d123112e3020ed4363f0c07c78
Douglas Lowder <douglowder@mac.com>
2026-05-06 16:02:37 -0700

    Add E2E test

--- packages/expo-updates/e2e/fixtures/project_files/maestro/updates-server/server.ts
+++ packages/expo-updates/e2e/fixtures/project_files/maestro/updates-server/server.ts
@@ -38,6 +38,7 @@ let messages: any[] = [];
 let responsesToServe: any[] = [];
 
 let updateRequest: Request | null = null;
+let assetRequest: Request | null = null;
 
 let manifestToServe: null = null;
 let manifestHeadersToServe: { [x: string]: any } | null = null;
@@ -82,6 +83,7 @@ function stop() {
   messages = [];
   responsesToServe = [];
   updateRequest = null;
+  assetRequest = null;
   manifestToServe = null;
   manifestHeadersToServe = null;
   serveChannel = null;
@@ -108,6 +110,8 @@ app.use(
     res: any,
     next: () => void
   ) => {
+    console.log('Requested static file: ', JSON.stringify(req.headers, null, 2));
+    assetRequest = req;
     requestedStaticFiles.push(path.basename(req.url));
     next();
   }
@@ -465,6 +469,16 @@ app.get('/last-request-headers', (_: Request, res: Response) => {
     return;
   }
   res.status(200).json(updateRequest.headers).end();
+  return;
+});
+
+app.get('/last-asset-request-headers', (_: Request, res: Response) => {
+  console.log('Last asset request headers: ', JSON.stringify(assetRequest?.headers, null, 2));
+  if (!assetRequest) {
+    res.status(404).send('No asset request');
+    return;
+  }
+  res.status(200).json(assetRequest.headers).end();
   return;
 });
 
